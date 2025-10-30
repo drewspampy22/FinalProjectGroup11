445764b21896d3960ce702a55f4b5da6399e1b3c
Drew Spampinato <spampiap@mail.uc.edu>
2025-10-30 16:39:34 -0400

    Drew's Webpage added

--- FinalProjectGroup11/Controllers/HomeController.cs
+++ FinalProjectGroup11/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace FinalProjectGroup11.Controllers
         {
             return View();
         }
+        public IActionResult DrewPage()
+        {
+            return View("DrewPage");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
