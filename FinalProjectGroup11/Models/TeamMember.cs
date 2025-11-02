de78ff17dee8756f30c829393a4dbf8992dc0a6e
MuyiwaAbim <106896437+MuyiwaAbim@users.noreply.github.com>
2025-11-02 18:36:21 -0500

    Changes by Muyiwa Abimbola

---
+++ FinalProjectGroup11/Models/TeamMember.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalProjectGroup11.Models
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string ProfileUrl { get; set; } 
+    }
+}
