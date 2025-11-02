bb84350958a3a2dff89aa3db0335678f258ce2f7
MuyiwaAbim <106896437+MuyiwaAbim@users.noreply.github.com>
2025-11-02 18:36:21 -0500

    Changes by Muyiwa Abimbola

---
+++ FinalProjectGroup11/Controllers/TeamController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinalProjectGroup11.Models;
+using System.Collections.Generic;
+
+
+namespace FinalProjectGroup11.Controllers
+{
+    public class TeamController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var teamMembers = new List<TeamMember>
+            {
+                new TeamMember { Name = "Drew Spampinato", Role = "Developer", Email = "spampiap@mail.uc.edu" },
+                new TeamMember { Name = "Harsh Patel", Role = "Developer", Email = "patelhj@mail.uc.edu" },
+                new TeamMember { Name = "Muyiwa Abimbola", Role = "Developer", Email = "abimbomj@mail.uc.edu" }
+            };
+
+            return View(teamMembers);
+        }
+    }
+}
