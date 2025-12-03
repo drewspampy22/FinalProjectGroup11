f4f7fb2015614da15b17d4cd6a49faa878462944
Drew Spampinato <spampiap@mail.uc.edu>
2025-12-03 16:59:38 -0500

    Added Database for Drew's Favorite Movies, Add-Migration Inital Update-Database

---
+++ FinalProjectGroup11/Migrations/20251203215036_MoviesList.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinalProjectGroup11.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoviesList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Name", "Rating", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Django Unchained", 5, 2012 },
+                    { 2, "Spider-Man: Across the Spider-Verse", 4, 2023 },
+                    { 3, "Predator Killer of Killers", 5, 2025 },
+                    { 4, "Baby Driver", 4, 2017 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
